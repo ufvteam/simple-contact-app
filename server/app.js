1c3e5af3fd5316fdb0d2064864193a9031c1b2e2
Hieu.Le <Hieu.Le@student.ufv.ca>
2021-03-26 14:17:20 -0700

    Set up express server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const PORT = 3000;
+
+const server = express();
+
+// Set the header
+server.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  next();
+});
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.listen(PORT, console.log(`Server is listening on PORT ${PORT}`));
